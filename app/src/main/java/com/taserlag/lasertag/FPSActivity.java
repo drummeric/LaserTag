15b1fa1c7dd065e3857ee0b8f6b19428b8d10e41
drummeric <Wolfson.Eric@gmail.com>
2016-01-11 16:10:49 -0500

    Camera should work on Android versions below Marshmellow. My phone is running Marshmellow and there's a special way you have to enable permissions (to be added later). However, you can manually enable them but going into the App Settings on the phone. Bug: Can't figure out how to pause/resume and have camera still working. To do: Permissions for Android 6.0+

--- app/src/main/java/com/taserlag/lasertag/FPSActivity.java
+++ app/src/main/java/com/taserlag/lasertag/FPSActivity.java
@@ -5,19 +5,42 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 public class FPSActivity extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fps);
+        initializeCamera();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Get the Camera instance as the activity achieves full user focus
+        if (mCamera == null) {
+            //mCamera = getCameraInstance();
+            initializeCamera(); // Local method to handle camera init
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void initializeCamera() {
         // Create an instance of Camera
-
         mCamera = getCameraInstance();
 
         // Create our Preview view and set it as the content of our activity.
@@ -26,34 +49,15 @@ public class FPSActivity extends AppCompatActivity {
         preview.addView(mPreview);
     }
 
-
-    public static Camera getCameraInstance(){
+    public static Camera getCameraInstance() {
         Camera c = null;
         try {
             c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
+        } catch (Exception e) {
+            Log.e("LaserTag", "Camera is not available", e);
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
     }
 
-
-    private boolean checkCameraHardware(Context context) {
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-
-
-
-
-
-
-
-
-}//class
+} //FPSActivity
