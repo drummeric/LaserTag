6df70af4d685385db5636cc3c4befa10bc50f637
drummeric <Wolfson.Eric@gmail.com>
2016-01-11 20:32:01 -0500

    Fixed: Permissions for Android 6.0+

--- app/src/main/java/com/taserlag/lasertag/FPSActivity.java
+++ app/src/main/java/com/taserlag/lasertag/FPSActivity.java
@@ -1,8 +1,11 @@
 package com.taserlag.lasertag;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,14 +13,16 @@ import android.widget.FrameLayout;
 
 public class FPSActivity extends AppCompatActivity {
 
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+
     private Camera mCamera;
     private CameraPreview mPreview;
+    private boolean cameraPermission = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fps);
-        initializeCamera();
     }
 
     @Override
@@ -39,14 +44,56 @@ public class FPSActivity extends AppCompatActivity {
         }
     }
 
-    private void initializeCamera() {
-        // Create an instance of Camera
-        mCamera = getCameraInstance();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-        // Create our Preview view and set it as the content of our activity.
-        mPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(mPreview);
+                    // permission was granted, yay!
+                    // Create an instance of Camera
+                    mCamera = getCameraInstance();
+
+                    // Create our Preview view and set it as the content of our activity.
+                    mPreview = new CameraPreview(this, mCamera);
+                    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+                    preview.addView(mPreview);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    System.exit(0);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void initializeCamera() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+
+                // MY_PERMISSIONS_REQUEST_CAMERA is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+        } else {
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
     }
 
     public static Camera getCameraInstance() {
