451795a7318d6675a6b7514ad5f97c5f965babcb
drummeric <Wolfson.Eric@gmail.com>
2016-01-12 12:15:16 -0500

    Fixed: Pause/resume works with Camera

--- app/src/main/java/com/taserlag/lasertag/FPSActivity.java
+++ app/src/main/java/com/taserlag/lasertag/FPSActivity.java
@@ -17,7 +17,6 @@ public class FPSActivity extends AppCompatActivity {
 
     private Camera mCamera;
     private CameraPreview mPreview;
-    private boolean cameraPermission = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +28,19 @@ public class FPSActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         // Get the Camera instance as the activity achieves full user focus
-        if (mCamera == null) {
-            //mCamera = getCameraInstance();
-            initializeCamera(); // Local method to handle camera init
+        initializeCamera();
+        mCamera = getCameraInstance();
+        if (mCamera != null) {
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
+        releaseCamera();
     }
 
     @Override
@@ -54,11 +53,6 @@ public class FPSActivity extends AppCompatActivity {
                     // permission was granted, yay!
                     // Create an instance of Camera
                     mCamera = getCameraInstance();
-
-                    // Create our Preview view and set it as the content of our activity.
-                    mPreview = new CameraPreview(this, mCamera);
-                    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-                    preview.addView(mPreview);
 
                 } else {
 
@@ -88,15 +82,10 @@ public class FPSActivity extends AppCompatActivity {
         } else {
             // Create an instance of Camera
             mCamera = getCameraInstance();
-
-            // Create our Preview view and set it as the content of our activity.
-            mPreview = new CameraPreview(this, mCamera);
-            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-            preview.addView(mPreview);
         }
     }
 
-    public static Camera getCameraInstance() {
+    private Camera getCameraInstance() {
         Camera c = null;
         try {
             c = Camera.open(); // attempt to get a Camera instance
@@ -105,6 +94,16 @@ public class FPSActivity extends AppCompatActivity {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+        if (mPreview != null) {
+            mPreview.getHolder().removeCallback(mPreview);
+        }
     }
 
 } //FPSActivity
