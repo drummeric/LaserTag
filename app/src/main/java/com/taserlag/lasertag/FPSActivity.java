510f0aee0d1b4ec62276fffac2a4b19f5c2f0194
drummeric <Wolfson.Eric@gmail.com>
2016-01-14 13:14:57 -0500

    Added: Map in bottom left Still needs lots of fixes

--- app/src/main/java/com/taserlag/lasertag/FPSActivity.java
+++ app/src/main/java/com/taserlag/lasertag/FPSActivity.java
@@ -1,8 +1,12 @@
 package com.taserlag.lasertag;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.MediaPlayer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -16,14 +20,25 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.VerticalSeekBar;
 
-public class FPSActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FPSActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
+    private final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 2;
 
     private Camera mCamera;
     private CameraPreview mPreview;
     private TextView mAmmo;
     private int ammo = 10;
+    private GoogleMap map;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +46,9 @@ public class FPSActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fps);
         mAmmo = (TextView) findViewById(R.id.ammo_text_view);
         mAmmo.setText(Integer.toString(ammo));
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -97,9 +115,26 @@ public class FPSActivity extends AppCompatActivity {
                 }
                 return;
             }
+            case MY_PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            // other 'case' lines to check for other
-            // permissions this app might request
+                    // permission was granted, yay!
+                    // Get the location
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+                    } else {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    System.exit(0);
+                }
+                return;
+            }
         }
     }
 
@@ -191,4 +226,41 @@ public class FPSActivity extends AppCompatActivity {
         mp.start();
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        map.clear();
+        MarkerOptions mp = new MarkerOptions();
+        mp.position(latLng).title("Current Location");
+        map.addMarker(mp);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 20);
+        map.animateCamera(cameraUpdate);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
 } //FPSActivity
