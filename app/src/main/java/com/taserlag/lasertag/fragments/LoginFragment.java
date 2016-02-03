47487d5e429f9b501c481a210a8a551a2b64bce1
zscott515 <zack.scott@live.com>
2016-02-03 13:53:11 -0500

    Added login input checks

--- app/src/main/java/com/taserlag/lasertag/fragments/LoginFragment.java
+++ app/src/main/java/com/taserlag/lasertag/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.taserlag.lasertag.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -19,6 +20,8 @@ import com.taserlag.lasertag.activity.MenuActivity;
 import com.taserlag.lasertag.application.LaserTagApplication;
 
 public class LoginFragment extends Fragment {
+
+    private final int MIN_INPUT_LENGTH = 5;
 
     private final String TAG = "LoginFragment";
     private OnFragmentInteractionListener mListener;
@@ -60,22 +63,65 @@ public class LoginFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
+    /*
+     * RULES:
+     * Username >= 5 characters long
+     * Password >= 5 characters long
+     */
     public void performLogin() {
-        final EditText username = ((EditText) getView().findViewById(R.id.edit_text_login_username));
-        final EditText password = ((EditText) getView().findViewById(R.id.edit_text_login_password));
-        LaserTagApplication.kinveyClient.user().login(username.getText().toString(), password.getText().toString(), new KinveyUserCallback() {
+        final EditText usernameText = ((EditText) getView().findViewById(R.id.edit_text_login_username));
+        final EditText passwordText = ((EditText) getView().findViewById(R.id.edit_text_login_password));
+        boolean validLoginCredentials = true;
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        usernameText.setError(null);
+        passwordText.setError(null);
+
+        if (username.length()<MIN_INPUT_LENGTH){
+            usernameText.setError(getString(R.string.login_error_username_invalid_length));
+            validLoginCredentials = false;
+        }
+
+        if (password.length()<MIN_INPUT_LENGTH){
+            passwordText.setError(getString(R.string.login_error_password_invalid_length));
+            validLoginCredentials = false;
+        }
+
+        if (validLoginCredentials){
+            doLogin(username,password);
+        } else {
+            clearEntries();
+        }
+
+    }
+
+    private void clearEntries(){
+        ((EditText) getView().findViewById(R.id.edit_text_login_username)).setText("");
+        ((EditText) getView().findViewById(R.id.edit_text_login_password)).setText("");
+    }
+
+    private void doLogin(String username,String password){
+        final ProgressDialog PD = new ProgressDialog(getActivity());
+        PD.setTitle("Please Wait..");
+        PD.setMessage("Loading...");
+        PD.setCancelable(false);
+        PD.show();
+
+        LaserTagApplication.kinveyClient.user().login(username, password, new KinveyUserCallback() {
+
             @Override
             public void onFailure(Throwable t) {
+                PD.dismiss();
                 Log.e(TAG, "Login Failure", t);
-                CharSequence text = "Wrong username or password.";
+                CharSequence text = getString(R.string.login_error_wrong_credentials);
                 Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-
-                username.setText("");
-                password.setText("");
+                clearEntries();
             }
 
             @Override
             public void onSuccess(User u) {
+                PD.dismiss();
                 Log.i(TAG,"Logged in a user with id: " + u.getId());
                 CharSequence text = "Welcome back, " + u.getUsername() + ".";
                 Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/taserlag/lasertag/fragments/LoginSignupFragment.java
+++ app/src/main/java/com/taserlag/lasertag/fragments/LoginSignupFragment.java
@@ -1,5 +1,6 @@
 package com.taserlag.lasertag.fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -19,6 +20,10 @@ import com.taserlag.lasertag.activity.MenuActivity;
 import com.taserlag.lasertag.application.LaserTagApplication;
 
 public class LoginSignupFragment extends Fragment {
+
+    private final int MIN_INPUT_LENGTH = 5;
+    private final int MIN_EMAIL_LENGTH = 6;
+    private final int MIN_NAME_LENGTH = 2;
 
     private final String TAG = "LoginSignupFragment";
     private OnFragmentInteractionListener mListener;
@@ -60,33 +65,100 @@ public class LoginSignupFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
+    /*
+     * RULES:
+     * username >= 5
+     * password >= 5
+     * passwordConfirm = password
+     * email contains @ and >= 6
+     * name >= 2
+     */
     public void performSignup() {
-        final EditText username = ((EditText) getView().findViewById(R.id.edit_text_signup_username));
-        final EditText password = ((EditText) getView().findViewById(R.id.edit_text_signup_password));
-        final EditText passwordConfirm = ((EditText) getView().findViewById(R.id.edit_text_signup_password_confirm));
-        final EditText email = ((EditText) getView().findViewById(R.id.edit_text_signup_email));
-        final EditText name = ((EditText) getView().findViewById(R.id.edit_text_signup_name));
+        final EditText usernameText = ((EditText) getView().findViewById(R.id.edit_text_signup_username));
+        final EditText passwordText = ((EditText) getView().findViewById(R.id.edit_text_signup_password));
+        final EditText passwordConfirmText = ((EditText) getView().findViewById(R.id.edit_text_signup_password_confirm));
+        final EditText emailText = ((EditText) getView().findViewById(R.id.edit_text_signup_email));
+        final EditText nameText = ((EditText) getView().findViewById(R.id.edit_text_signup_name));
+        boolean validSignupCredentials = true;
 
-        if (!password.getText().toString().equals(passwordConfirm.getText().toString())) {
-            passwordConfirm.setError("Your passwords must match");
-            return;
+        usernameText.setError(null);
+        passwordText.setError(null);
+        passwordConfirmText.setError(null);
+        emailText.setError(null);
+        nameText.setError(null);
+
+        String username = usernameText.getText().toString();
+        String password = passwordText.getText().toString();
+        String passwordConfirm = passwordConfirmText.getText().toString();
+        String email = emailText.getText().toString();
+        String name = nameText.getText().toString();
+
+        if (username.length()<MIN_INPUT_LENGTH){
+            usernameText.setError(getString(R.string.login_error_username_invalid_length));
+            validSignupCredentials = false;
         }
 
-        LaserTagApplication.kinveyClient.user().create(username.getText().toString(), password.getText().toString(), new KinveyUserCallback() {
+        if (password.length()<MIN_INPUT_LENGTH){
+            passwordText.setError(getString(R.string.login_error_password_invalid_length));
+            validSignupCredentials = false;
+        }
+
+        if (!password.equals(passwordConfirm)) {
+            passwordConfirmText.setError(getString(R.string.signup_error_mismatch_passwords));
+            passwordText.setText("");
+            passwordConfirmText.setText("");
+            validSignupCredentials = false;
+        }
+
+        if (!email.contains("@") || !(email.contains(".")) || email.length() < MIN_EMAIL_LENGTH){
+            emailText.setError(getString(R.string.signup_error_email_invalid));
+            emailText.setText("");
+            validSignupCredentials = false;
+        }
+
+        if (name.length() < MIN_NAME_LENGTH){
+            nameText.setError(getString(R.string.signup_error_name_invalid));
+            emailText.setText("");
+            validSignupCredentials = false;
+        }
+
+        if (validSignupCredentials){
+            doSignup(username,password,email,name);
+        }
+    }
+
+    private void doSignup(String username, String password, final String email, final String name){
+
+        final ProgressDialog PD = new ProgressDialog(getActivity());
+        PD.setTitle("Please Wait..");
+        PD.setMessage("Loading...");
+        PD.setCancelable(false);
+        PD.show();
+
+        LaserTagApplication.kinveyClient.user().create(username, password, new KinveyUserCallback() {
             @Override
             public void onFailure(Throwable t) {
+                PD.dismiss();
                 Log.e(TAG, "Signup Failure", t);
-                CharSequence text = "Could not sign up.";
-                Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                Log.e(TAG, t.getMessage());
+
+                if (t.getMessage().startsWith("UserAlreadyExists")){
+                    CharSequence text = getString(R.string.signup_error_username_conflict);
+                    Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                } else {
+                    CharSequence text = getString(R.string.signup_failure);
+                    Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                }
             }
             @Override
             public void onSuccess(User u) {
+                PD.dismiss();
                 Log.i(TAG, "Signed up a user with id: " + u.getId());
                 CharSequence text = u.getUsername() + ", your account has been created.";
                 Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_SHORT).show();
 
-                LaserTagApplication.kinveyClient.user().put("email", email.getText().toString());
-                LaserTagApplication.kinveyClient.user().put("first_name", name.getText().toString());
+                LaserTagApplication.kinveyClient.user().put("email", email);
+                LaserTagApplication.kinveyClient.user().put("first_name", name);
                 LaserTagApplication.kinveyClient.user().update(new KinveyUserCallback() {
                     @Override
                     public void onFailure(Throwable e) {Log.e(TAG, "Failed to set up user fields", e);}
